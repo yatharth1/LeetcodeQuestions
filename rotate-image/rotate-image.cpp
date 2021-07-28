f48666ad0275f9acb183cc5c8f38bce47f7a985c
Yatharth Goyal <yatharthgoyal1@gmail.com>
2021-07-28 20:32:00 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (70.80%) - LeetHub

---
+++ rotate-image/rotate-image.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        
+        int n = matrix.size();
+        int m = matrix[0].size();
+        
+        for(int i = 0; i < n; i++)
+        {
+            for(int j = i + 1; j < m; j++){
+                swap(matrix[i][j], matrix[j][i]);
+            }
+        }
+        
+        for(int i = 0; i < m; i++)
+        {
+            int low = 0, high = m - 1;
+            while(low < high){
+                swap(matrix[i][low], matrix[i][high]);
+                low++;
+                high--;
+            }
+        }
+    }
+};
